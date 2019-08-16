55f8938c33fc5230c40b50ca50153c72e514f702
Saul Johnson <saul.a.johnson@gmail.com>
2019-08-16 16:21:57 +0100

    Add error introduction script

---
+++ src/introduceerrors.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import sys
+
+
+def print_usage(show_help_line=False):
+    """ Prints the short help card for the program.
+    Args:
+        show_help_line (bool): If true, information on help flag `-h` will be printed.
+    """
+    print("Usage: python introduceerrors.py [-h] <dumpfile>")
+    print("Introduces errors to a password dump formatted as a CSV file.")
+    if show_help_line:
+        print("For extended help use '-h' option.")
+
+
+def print_help():
+    """ Prints the full help card for the program.
+    """
+    print_usage()
+    print("Options:")
+    print("\t-h: Show this help screen")
+    print()
+    print("Input file should be in format:")
+    print("\tpassword, frequency, ... <- Column headers")
+    print("\t\"123456\", 1, ...")
+    print("\t\"password\", 18, ...")
+    print("\t\"matrix\", 14, ...")
+    print("Output will be in CSV format to standard output.")
+    print()
+    print("Any passwords containing commas or spaces will be split into multiple records.")
+
+
+# Read in CSV file.
+df = pd.read_csv(sys.argv[-1], skipinitialspace=True)
+
+# Print header.
+print('password, frequency')
+
+# For each row in dump.
+for index, row in df.iterrows():
+  fragments = row['password'].split(' ')
+  fragments = list(map(lambda x: x.split(','), fragments)) # Split along spaces and commas.
+  fragments = [item for sublist in fragments for item in sublist] # Flatten list.
+  for fragment in fragments:
+    print('"' + fragment.replace('"', '""') + '",', row['frequency']) # Output row.
